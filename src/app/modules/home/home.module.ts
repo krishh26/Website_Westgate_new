ea560029c6f88db27df8e19a2c0ebfecc4bccb2e
darshan <darshandumaraliya@gmail.com>
2025-01-31 01:05:44 +0530

    Changes done

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -4,6 +4,7 @@ import { HomeComponent } from './home/home.component';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent }
 ];
 

368676ee61c9be0417112d7b6eec8a5152d7d55c
Kishansinh Parmar <kishansinhp26@gmail.com>
2025-01-31 12:07:40 +0530

    changes done

--- src/app/modules/blogs/blogs.module.ts
+++ src/app/modules/blogs/blogs.module.ts
@@ -2,14 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BlogsComponent } from './blogs.component';
 import { RouterModule, Routes } from '@angular/router';
+import { BlogDetailComponent } from './blog-detail/blog-detail.component';
 
 const routes: Routes = [
-  { path: '', component: BlogsComponent }
+  { path: '', component: BlogsComponent },
+  { path: 'blog-detail', component: BlogDetailComponent }
 ];
 
 @NgModule({
   declarations: [
-    BlogsComponent
+    BlogsComponent,
+    BlogDetailComponent
   ],
   imports: [
     CommonModule,

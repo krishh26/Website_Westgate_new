466fa0628ba1a749f961f5394abb6f0c7f2835cd
Kishansinh Parmar <kishansinhp26@gmail.com>
2025-02-21 16:24:53 +0530

    changes doen

--- src/app/shared/component/header/header.component.ts
+++ src/app/shared/component/header/header.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
-
+import { NavigationEnd, Router } from '@angular/router';
+declare var bootstrap: any;
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  isNavbarCollapsed = true;
+  private navbarCollapse: any;
+
+  ngAfterViewInit(): void {
+    this.navbarCollapse = new bootstrap.Collapse('#navbarNav', { toggle: false });
+  }
+
+  closeNavbar() {
+    if (this.navbarCollapse) {
+      this.navbarCollapse.hide(); // Properly close the Bootstrap navbar
+    }
+  }
+  constructor(private router: Router) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.isNavbarCollapsed = true; // Close navbar on route change
+        window.scrollTo(0, 0); // Ensure the page starts at the top
+      }
+    });
+  }
 
 }
